cb2e0941dad4c06d420bc58ab887fb3bdd74fd06
JohnYagecic <JYagecic@gmail.com>
2015-03-18 10:36:58 -0400

    extensive rewrite.  Replaced date with time most places, changed from linear process to loop, once through for each site

--- PlotNonTidalFlows2.R
+++ PlotNonTidalFlows2.R
@@ -1,73 +1,56 @@
 setwd("~/AutoModelR")
 
-DelQ<-read.csv("Discharge_Delaware_Trenton.csv")
-DelQ$Date<-paste0(DelQ$Date, " 0:0:0")
-DelQ$Date<-as.Date(DelQ$Date, format="%Y-%m-%d %H:%M:%S")
+for (jjj in 1:2){
+   # Two loops through, first for Delaware, second for Schuylkill
+  if (jjj==1){
+    NWISfile="Discharge_Delaware_Trenton.csv"
+    AHPSfile="DelawareAHPS.csv"
+    PNGout="DelawareNonTidal.png"
+    PlotTitle="Delaware at Trenton, Observed and Forecasted Discharge"
+  }
+  
+  if (jjj==2){
+    NWISfile="Discharge_Schuylkill_Philadelphia.csv"
+    AHPSfile="SchuylkillAHPS.csv"
+    PNGout="SchuylkillNonTidal.png"
+    PlotTitle="Schuylkill at Philadelphia, Observed and Forecasted Discharge"
+  }
 
-head(DelQ)
-
-DelAHPS<-read.csv("DelawareAHPS.csv")
-DelAHPS$Date.Time<-as.Date(DelAHPS$Date.Time, format="%Y-%m-%d %H:%M:%S")
-
-DelObs<-DelQ[(DelQ$Date>(Sys.Date()-20)&DelQ$Flag!="Replaced NA"),]
-head(DelObs)
-DelAll<-DelObs[,c(3,5)]
-
-DelNA<-DelQ[(DelQ$Date>(Sys.Date()-20)&DelQ$Flag=="Replaced NA"),]
-head(DelNA)
-DelAll<-rbind(DelAll, DelNA[,c(3,5)])
-names(DelAll)<-c("Date", "Discharge")
-
-lastDate<-max(DelQ$Date)
-DelAHPStemp<-DelAHPS[(DelAHPS$Date.Time>lastDate),c(1,3)]
-DelAHPStemp$Flag<-"AHPS"
-names(DelAHPStemp)<-c("Date", "Discharge", "Flag")
-head(DelAHPStemp)
-DelAll<-rbind(DelAll, DelAHPStemp[,1:2])
-DelAll<-DelAll[order(DelAll$Date),]
-
-png(file="DelawareNonTidal.png")
-plot(DelAll$Date, DelAll$Discharge, col="black", type="l",
-     xlim=c(Sys.Date()-20, Sys.Date()+5),
-     xlab="Date & Time", ylab="Discharge (CFS)",
-     main="Delaware at Trenton, Observed and Forecasted Discharge")
-points(DelObs$Date, DelObs$Discharge.CFS, col="blue", pch=19)
-points(DelNA$Date, DelNA$Discharge.CFS, col="red", pch=19)
-points(DelAHPStemp$Date, DelAHPStemp$Discharge, col="green", pch=19)
-legend("topleft", legend=c("Obs", "NA", "AHPS"), col=c("blue", "red", "green"), pch=19)
-dev.off()
-
-#  Schuylkill River
-
-SchQ<-read.csv("Discharge_Schuylkill_Philadelphia.csv")
-SchQ$Date<-paste0(SchQ$Date, " 0:0:0")
-SchQ$Date<-as.Date(SchQ$Date, format="%Y-%m-%d %H:%M:%S")
-
-SchAHPS<-read.csv("SchuylkillAHPS.csv")
-SchAHPS$Date.Time<-as.Date(SchAHPS$Date.Time, format="%Y-%m-%d %H:%M:%S")
-
-SchObs<-SchQ[(SchQ$Date>(Sys.Date()-20)&SchQ$Flag!="Replaced NA"),]
-SchAll<-SchObs[,c(3,5)]
-
-SchNA<-SchQ[(SchQ$Date>(Sys.Date()-20)&SchQ$Flag=="Replaced NA"),]
-SchAll<-rbind(SchAll, SchNA[,c(3,5)])
-names(SchAll)<-c("Date", "Discharge")
-
-lastDate<-max(SchQ$Date)
-SchAHPStemp<-SchAHPS[(SchAHPS$Date.Time>lastDate),c(1,3)]
-SchAHPStemp$Flag<-"AHPS"
-names(SchAHPStemp)<-c("Date", "Discharge", "Flag")
-SchAll<-rbind(SchAll, SchAHPStemp[,1:2])
-SchAll<-SchAll[order(SchAll$Date),]
-
-png(file="SchuylkillNonTidal.png")
-plot(SchAll$Date, SchAll$Discharge, col="black", type="l",
-     xlim=c(Sys.Date()-20, Sys.Date()+5),
-     pch=19, xlab="Date & Time", ylab="Discharge (CFS)",
-     main="Schuylkill at Philadelphia, Observed and Forecasted Discharge")
-points(SchObs$Date, SchObs$Discharge.CFS, col="blue", pch=19)
-points(SchNA$Date, SchNA$Discharge.CFS, col="red", pch=19)
-points(SchAHPStemp$Date, SchAHPStemp$Discharge, col="green", pch=19)
-legend("topleft", legend=c("Obs", "NA", "AHPS"), col=c("blue", "red", "green"), pch=19)
-dev.off()
-
+  RivQ<-read.csv(NWISfile)                  # NWIS flows are provided by Date only, need time
+  RivQ$Date<-paste0(RivQ$Date, " 00:00:01") # Add one second to force retention of POSIX class
+  
+  RivQ$Date<-strptime(RivQ$Date, format("%Y-%m-%d %H:%M:%S"))
+  
+  RivAHPS<-read.csv(AHPSfile)
+  RivAHPS$Date.Time<-strptime(RivAHPS$Date.Time, format("%Y-%m-%d %H:%M:%S"))
+  
+  # Lines below separate NWIS data into observations and extrapolations of last value
+  # for NA data (such as ice or meter malfunction)
+  RivObs<-RivQ[(as.Date(RivQ$Date)>(Sys.Date()-20)&RivQ$Flag!="Replaced NA"),]
+  RivAll<-RivObs[,c(3,5)] # RivAll is needed to plot continuous black line in plot
+  
+  RivNA<-RivQ[(as.Date(RivQ$Date)>(Sys.Date()-20)&RivQ$Flag=="Replaced NA"),]
+  RivAll<-rbind(RivAll, RivNA[,c(3,5)])
+  names(RivAll)<-c("Date", "Discharge")
+  
+  lastDate<-max(RivQ$Date) # mark last NWIS value
+  RivAHPStemp<-RivAHPS[(RivAHPS$Date.Time>lastDate),c(1,3)] # cut off AHPS older than most recent NWIS
+  RivAHPStemp$Flag<-"AHPS"
+  names(RivAHPStemp)<-c("Date", "Discharge", "Flag")
+  RivAll<-rbind(RivAll, RivAHPStemp[,1:2]) # 
+  RivAll<-RivAll[order(RivAll$Date),]
+  
+  PlotTitle=paste0(PlotTitle, "\nData Retrieved ", Sys.Date()) # add generation date to plot title
+  
+  png(file=PNGout)
+  plot(RivAll$Date, RivAll$Discharge, col="black", type="l",
+       xlim=c(Sys.time()-(20*24*60*60), Sys.time()+(5*24*60*60)),
+       xlab="Date & Time", ylab="Discharge (CFS)",
+       main=PlotTitle) # black line from RivAll
+  points(RivObs$Date, RivObs$Discharge.CFS, col="blue", pch=19) # color points and legend from
+  points(RivNA$Date, RivNA$Discharge.CFS, col="red", pch=19)    # subsets
+  points(RivAHPStemp$Date, RivAHPStemp$Discharge, col="green", pch=19)
+  legend("topleft", legend=c("Obs", "NA", "AHPS"), col=c("blue", "red", "green"), pch=19)
+  dev.off()
+ 
+}
